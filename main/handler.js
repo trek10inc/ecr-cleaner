c561e177bedcd8b5ecaaa9aa4e1a25261365fdc5
Jared Short <jaredlshort@gmail.com>
2016-04-14 15:59:20 -0400

    Tabs -> Spaces for a better 🌎

--- main/handler.js
+++ main/handler.js
@@ -5,48 +5,48 @@ var lib = require('./lib');
 // Lambda Handler
 module.exports.handler = function(event, context) {
 
-	// Check event for dry run, which prevents actual deletion
-	// check logs to see what would have been deleted
-	console.log(event);
-	if(event.dryRun){
-		process.env.DRY_RUN = true;
-	} else {
-		process.env.DRY_RUN = false;
-	}
+  // Check event for dry run, which prevents actual deletion
+  // check logs to see what would have been deleted
+  console.log(event);
+  if(event.dryRun){
+    process.env.DRY_RUN = true;
+  } else {
+    process.env.DRY_RUN = false;
+  }
 
-	if(!process.env.AWS_ACCOUNT_ID){
-		console.warn('WARN: NO AWS_ACCOUNT_ID, defaulting to current account');
-		process.env.AWS_ACCOUNT_ID = context.invoked_function_arn.split(':')[4];
-	}
+  if(!process.env.AWS_ACCOUNT_ID){
+    console.warn('WARN: NO AWS_ACCOUNT_ID, defaulting to current account');
+    process.env.AWS_ACCOUNT_ID = context.invoked_function_arn.split(':')[4];
+  }
 
-	if(!process.env.REPO_REGION){
-		console.warn('WARN: NO REPO_REGION, defaulting to us-east-1');
-		process.env.AWS_REGION = 'us-east-1';
-	}
+  if(!process.env.REPO_REGION){
+    console.warn('WARN: NO REPO_REGION, defaulting to us-east-1');
+    process.env.AWS_REGION = 'us-east-1';
+  }
 
-	if(!process.env.ECS_REGION){
-		console.warn('WARN: NO ECS_REGION, defaulting to us-east-1');
-		process.env.AWS_REGION = 'us-east-1';
-	}
+  if(!process.env.ECS_REGION){
+    console.warn('WARN: NO ECS_REGION, defaulting to us-east-1');
+    process.env.AWS_REGION = 'us-east-1';
+  }
 
-	if(!process.env.REPO_AGE_THRESHOLD){
-		console.warn('WARN: NO REPO_AGE_THRESHOLD, defaulting to 90 days');
-		process.env.AWS_REGION = 90;
-	}
+  if(!process.env.REPO_AGE_THRESHOLD){
+    console.warn('WARN: NO REPO_AGE_THRESHOLD, defaulting to 90 days');
+    process.env.AWS_REGION = 90;
+  }
 
-	if(!process.env.REPO_TO_CLEAN){
-		console.error('WARN: NO REPO_TO_CLEAN, defaulting to us-east-1');
-		return context.fail(new Error('Must set REPO_TO_CLEAN'));
-	}
+  if(!process.env.REPO_TO_CLEAN){
+    console.error('WARN: NO REPO_TO_CLEAN, defaulting to us-east-1');
+    return context.fail(new Error('Must set REPO_TO_CLEAN'));
+  }
 
-	var params = {
-		repositoryName: process.env.REPO_TO_CLEAN
-	};
+  var params = {
+    repositoryName: process.env.REPO_TO_CLEAN
+  };
 
-	lib.getRepoImages(params)
-		.then(lib.filterImagesByDateThreshold)
-		.then(lib.filterOutActiveImages)
-		.then(lib.deleteImages)
-		.then(context.succeed)
-		.catch(context.fail);
+  lib.getRepoImages(params)
+    .then(lib.filterImagesByDateThreshold)
+    .then(lib.filterOutActiveImages)
+    .then(lib.deleteImages)
+    .then(context.succeed)
+    .catch(context.fail);
 };
