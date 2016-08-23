4a00903b4fe390bde0064b0d61058ad37d92cf00
Charlie <Charlie>
2016-08-23 16:07:32 -0400

    Added pagination to ecr.listImages and ecs.listTaskDefinitions.

--- main/lib/index.js
+++ main/lib/index.js
@@ -5,8 +5,10 @@ var Promise = require('bluebird');
 var _ = require('lodash');
 
 
-var ecr = Promise.promisifyAll(new AWS.ECR({region: process.env.REPO_REGION, maxRetries: 3}));
-var ecs = Promise.promisifyAll(new AWS.ECS({region: process.env.ECS_REGION, maxRetries: 3}));
+var ecr = Promise.promisifyAll(new AWS.ECR({ region: process.env.REPO_REGION, maxRetries: 3 }));
+var ecs = Promise.promisifyAll(new AWS.ECS({ region: process.env.ECS_REGION, maxRetries: 3 }));
+
+var ECS_CONCURRENCY = 10;
 
 /**
  * Lib
@@ -19,32 +21,49 @@ function getImageAgeDays(timestamp) {
   var d = parseInt(timestamp.split('-')[2].split('T')[0]);
   var h = parseInt(timestamp.split('T')[1].split(':')[0]);
 
-  var age = Date.now() - Date.UTC(y,m,d,h);
+  var age = Date.now() - Date.UTC(y, m, d, h);
   var days = 1000 * 60 * 60 * 24;
-  age = Math.round(age/days);
+  age = Math.round(age / days);
 
   return (age);
 }
 
-exports.getRepoImages = function(params){
-  return ecr.listImagesAsync(params);
+function iterateImages(params, previousResults) {
+  if (!previousResults) {
+    previousResults = [];
+  }
+
+  return ecr.listImagesAsync(params)
+    .then(function (data) {
+      previousResults = previousResults.concat(data.imageIds);
+      if (data.nextToken) {
+        params.nextToken = data.nextToken;
+        return iterateImages(params, previousResults);
+      } else {
+        return { imageIds: previousResults };
+      }
+    });
+}
+
+exports.getRepoImages = function (params) {
+  return iterateImages(params);
 };
 
 
 // Takes an argument of an array of full image definitions
 //   and splits into tags to delete from the REPO_TO_CLEAN repo
-exports.deleteImages = function(images){
-  return new Promise(function(resolve){
+exports.deleteImages = function (images) {
+  return new Promise(function (resolve) {
     console.info('IMAGES TO DELETE:', images);
-    var imageTagsToDelete = _.map(images, function(image){
-      return {imageTag: image.split(':')[1]};
+    var imageTagsToDelete = _.map(images, function (image) {
+      return { imageTag: image.split(':')[1] };
     });
 
     console.info('IMAGE TAGS TO DELETE:', imageTagsToDelete);
 
     // Make sure we are doing this for real
-    if(process.env.DRY_RUN !== 'true'){
-      if(imageTagsToDelete.length > 0){
+    if (process.env.DRY_RUN !== 'true') {
+      if (imageTagsToDelete.length > 0) {
         var params = {
           imageIds: imageTagsToDelete,
           repositoryName: process.env.REPO_TO_CLEAN
@@ -52,62 +71,79 @@ exports.deleteImages = function(images){
 
         // Batch delete all images in the deletionQueue
         ecr.batchDeleteImageAsync(params)
-            .then(function(deletions){
-              resolve({
-                  failures: deletions.failures,
-                  imagesDeleted: deletions.imageIds,
-                  count:_.keys(deletions.imageIds).length
-                });
+          .then(function (deletions) {
+            resolve({
+              failures: deletions.failures,
+              imagesDeleted: deletions.imageIds,
+              count: _.keys(deletions.imageIds).length
             });
+          });
       } else {
         resolve({
-            failures: [],
-            imagesDeleted: [],
-            count:0
-          });
+          failures: [],
+          imagesDeleted: [],
+          count: 0
+        });
       }
-    } else{
+    } else {
       // Fake Results
       resolve({
-          dryRun: true,
-          failures: [],
-          imagesDeleted: imageTagsToDelete,
-          count:imageTagsToDelete.length
-        });
+        dryRun: true,
+        failures: [],
+        imagesDeleted: imageTagsToDelete,
+        count: imageTagsToDelete.length
+      });
     }
   });
 };
 
+function iterateTaskDefinitions(params, previousResults) {
+  if (!previousResults) {
+    previousResults = [];
+  }
+
+  return ecs.listTaskDefinitionsAsync(params)
+    .then(function (data) {
+      previousResults = previousResults.concat(data.taskDefinitionArns);
+      if (data.nextToken) {
+        params.nextToken = data.nextToken;
+        return iterateTaskDefinitions(params, previousResults);
+      } else {
+        return { taskDefinitionArns: previousResults };
+      }
+    });
+}
+
 // Goes through all of ECS in a particular region and determines what is still
 //   marked as active and in use containers at a Task level (not actual running tasks)
-exports.filterOutActiveImages = function(eligibleForDeletion){
+exports.filterOutActiveImages = function (eligibleForDeletion) {
   console.info('BEFORE FILTER:', eligibleForDeletion);
 
-  return ecs.listTaskDefinitionsAsync({ status: 'ACTIVE' })
-    .then(function(taskDefs){
-      return new Promise(function(resolve){
-        Promise.map(taskDefs.taskDefinitionArns, function(taskDefinitionARN) {
-
-            // Get all active images from all container defintions
-            return ecs.describeTaskDefinitionAsync({taskDefinition: taskDefinitionARN})
-              .then(function(taskDefinitionDetails){
-                return _.chain(taskDefinitionDetails)
-                  .map(function(taskDefinitionDetail){
-                    return taskDefinitionDetail.containerDefinitions;
-                  })
-                  .map(function(containerDefinition){
-                    return _.map(containerDefinition, 'image');
-                  }).value();
-              });
-        }).then(resolve);
+  return iterateTaskDefinitions({ status: 'ACTIVE' })
+    .then(function (taskDefs) {
+      return new Promise(function (resolve) {
+        Promise.map(taskDefs.taskDefinitionArns, function (taskDefinitionARN) {
+          // Get all active images from all container defintions
+          return ecs.describeTaskDefinitionAsync({ taskDefinition: taskDefinitionARN })
+            .then(function (taskDefinitionDetails) {
+              return _.chain(taskDefinitionDetails)
+                .map(function (taskDefinitionDetail) {
+                  return taskDefinitionDetail.containerDefinitions;
+                })
+                .map(function (containerDefinition) {
+                  return _.map(containerDefinition, 'image');
+                }).value();
+            });
+        }, {concurrency: ECS_CONCURRENCY}).then(resolve);
+        // concurrency set so that ecs doesn't get upset about a lot of calls very quickly
       });
-    }).then(function(allImages){
+    }).then(function (allImages) {
       // Then reduce the big array and reduce to uniq values
       var activeImages = _.chain(allImages).flattenDeep().uniq().value();
-      console.log('ACTIVE IMAGES:', activeImages);
+      console.info('ACTIVE IMAGES:', activeImages);
 
       // Remove images from deletion that are active
-      return  _.difference(eligibleForDeletion, activeImages);
+      return _.difference(eligibleForDeletion, activeImages);
     });
 };
 
@@ -115,7 +151,7 @@ exports.filterOutActiveImages = function(eligibleForDeletion){
 // Fetch all layers / image details from the repo
 // Filter out everything newer than some variable amount of days
 //   set via REPO_AGE_THRESHOLD (90 days by default)
-exports.filterImagesByDateThreshold = function(images){
+exports.filterImagesByDateThreshold = function (images) {
   console.info('IMAGES TO PROCESS:', images);
   var params = {
     imageIds: images.imageIds,
@@ -123,32 +159,32 @@ exports.filterImagesByDateThreshold = function(images){
   };
 
   return ecr.batchGetImageAsync(params)
-    .then(function(imageDetails){
+    .then(function (imageDetails) {
 
-        // Get all tags eligible for deletion by age threshold
-        //   coerce each of the tags to a full image reference for easy comparison
-        var eligibleForDeletion = _.map(imageDetails.images, function(image){
+      // Get all tags eligible for deletion by age threshold
+      //   coerce each of the tags to a full image reference for easy comparison
+      var eligibleForDeletion = _.map(imageDetails.images, function (image) {
 
-            var created = JSON.parse(JSON.parse(image.imageManifest)
-                                         .history[0]
-                                         .v1Compatibility).created;
+        var created = JSON.parse(JSON.parse(image.imageManifest)
+          .history[0]
+          .v1Compatibility).created;
 
-            var imageTag = JSON.parse(image.imageManifest).tag;
+        var imageTag = JSON.parse(image.imageManifest).tag;
 
-            if(created &&
-               imageTag !== 'latest' &&
-               getImageAgeDays(created) >= process.env.REPO_AGE_THRESHOLD){
-                 return process.env.AWS_ACCOUNT_ID +
-                         '.dkr.ecr.' +
-                         process.env.REPO_REGION +
-                         '.amazonaws.com/' +
-                         process.env.REPO_TO_CLEAN +
-                         ':' + imageTag;
-            } else {
-              return null;
-            }
-        });
+        if (created &&
+          imageTag !== 'latest' &&
+          getImageAgeDays(created) >= process.env.REPO_AGE_THRESHOLD) {
+          return process.env.AWS_ACCOUNT_ID +
+            '.dkr.ecr.' +
+            process.env.REPO_REGION +
+            '.amazonaws.com/' +
+            process.env.REPO_TO_CLEAN +
+            ':' + imageTag;
+        } else {
+          return null;
+        }
+      });
 
-        return _.compact(eligibleForDeletion);
+      return _.compact(eligibleForDeletion);
     });
 };
